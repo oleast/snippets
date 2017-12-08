19be75ce346a4511993cb0de7545f5db7dcc5fec
oleast <oleast@stud.ntnu.no>
2017-12-08 13:39:48 +0100

    Finished TDT4110 2013h t3c

--- tdt4110/2013h/t3.py
+++ tdt4110/2013h/t3.py
@@ -20,12 +20,58 @@ def secret2(m):
     else:
         return -1
 
+def secret3(code):
+    L = len(code) - 1
+    if L >= 0:
+        if code[L] == '0':
+            decode = '0000'
+        elif code[L] == '1':
+            decode = '0001'
+        elif code[L] == '2':
+            decode = '0001'
+        elif code[L] == '3':
+            decode = '0011'
+        elif code[L] == '4':
+            decode = '0100'
+        elif code[L] == '5':
+            decode = '0101'
+        elif code[L] == '6':
+            decode = '0110'
+        elif code[L] == '7':
+            decode = '0111'
+        elif code[L] == '8':
+            decode = '1000'
+        elif code[L] == '9':
+            decode = '1001'
+        elif code[L] == 'A':
+            decode = '1010'
+        elif code[L] == 'B':
+            decode = '1011'
+        elif code[L] == 'C':
+            decode = '1100'
+        elif code[L] == 'D':
+            decode = '1101'
+        elif code[L] == 'E':
+            decode = '1110'
+        elif code[L] == 'F':
+            decode = '1111'
+        else:
+            decode = 'XXXX'
+        if L == 0:
+            return decode
+        else:
+            return secret3(code[0:L]) + decode
+    else:
+        return ''
+
 def main():
     print(secret1(11, 3))
 
     m = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     answer = secret2(m)
     print(answer)
+
+    print(secret3('148'))
 
 if __name__ == "__main__":
     main()
